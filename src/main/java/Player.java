bcc3626e1efedc4898338ffba3db8647c519de27
Pedram Valiani <p.valiani@gmail.com>
2021-01-30 17:25:20 +0000

    text comment updates in player

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -11,6 +11,7 @@ public class Player {
         this.hand = hand;
     }
 
+//  Constructor defines a player for the game
     public Player(String name, Card hand){
         this.name = name;
         this.hand = new ArrayList<Card>();
@@ -21,15 +22,17 @@ public class Player {
         return hand;
     }
 
+//    getName method gets player name
     public String getName() {
         return this.name;
     }
 
-
+// sets player name
     public void setName(String name) {
         this.name = name;
     }
 
+//  adds a card to the players hand
     public void addCardToHand(Card card) {
         this.hand.add(card);
     }
